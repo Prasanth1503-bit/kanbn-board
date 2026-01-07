5f91b840fca1ad69f7990dc4e3786144df4036a0
Prasanth1503-bit <15037777gpp@gmail.com>
2026-01-07 20:59:39 +0530

    craeted a taskdat array and add task button functionally making

--- kanbn.js
+++ kanbn.js
@@ -1,4 +1,4 @@
-
+import { toDoTasksData, inProgressTasksData, doneTasksData } from "./tasksdata.js";
 const filterButton = document.getElementById('filter-button');
 const filterTasks = document.getElementById('filter-tasks');
 
@@ -25,3 +25,34 @@ window.onclick = function (event) {
 document.querySelector('#theme-toggle').addEventListener('click', function () {
     document.body.classList.toggle('dark-mode');
 });
+function renderTasks(taskType) {
+    let taskBox = '';
+   taskType.forEach((task) => {
+        taskBox += `<div class=" task-box">
+            <p>${task.name}</p>
+            <p> ${task.date}</p>
+        </div>`
+    })
+    return taskBox;
+}
+function addTaskButton(taskType,tasksdata,taskbar){
+         taskType.addEventListener('click', () => {
+    tasksdata.push({name:"Buy groceries",
+        date:"jan 6 2026"})
+    taskbar.innerHTML = renderTasks(tasksdata);
+});
+}
+
+const toDoTasksBar = document.querySelector('.to-do');
+const inProgressTasksBar = document.querySelector('.in-progress');
+const doneTasksBar = document.querySelector('.done');
+toDoTasksBar.innerHTML = renderTasks(toDoTasksData);
+inProgressTasksBar.innerHTML = renderTasks(inProgressTasksData);
+doneTasksBar.innerHTML = renderTasks(doneTasksData);
+const toDoAddButton = document.querySelector('.do-task-add-button');
+const inProgressAddButton = document.querySelector('.in-progress-add-button');
+const doneAddButton = document.querySelector('.done-add-button');
+addTaskButton(toDoAddButton,toDoTasksData,toDoTasksBar);
+addTaskButton(inProgressAddButton,inProgressTasksData,inProgressTasksBar);
+addTaskButton(doneAddButton,doneTasksData,doneTasksBar);   
+ 
---
+++ tasksdata.js
@@ -0,0 +1,48 @@
+ //export { toDoTasksData, inProgressTasksData, doneTasksData };
+ export const toDoTasksData = [  
+    {
+        name:"Buy groceries",
+        date:"jan 6 2026"
+    },
+   
+     {
+        name:"Buy groceries",
+        date:"jan 6 2026"
+    }
+]
+ export const inProgressTasksData = [  
+    {
+        name:"Work on project",
+        date:"jan 6 2026"
+    },
+     {
+        name:"Work on project",
+        date:"jan 6 2026"
+    },
+     {
+        name:"Buy groceries",
+        date:"jan 6 2026"
+    },
+     {
+        name:"Buy groceries",
+        date:"jan 6 2026"
+    }
+]
+ export  const doneTasksData = [  
+    {
+        name:"Buy groceries",
+        date:"jan 6 2026"
+    },
+     {
+        name:"Buy groceries",
+        date:"jan 6 2026"
+    },
+     {
+        name:"Buy groceries",
+        date:"jan 6 2026"
+    },
+     {
+        name:"Buy groceries",
+        date:"jan 6 2026"
+    }
+]
